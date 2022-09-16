bf8f17374ea3cb8c6aaf0ebe627d02f9b96762fa
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-09-16 09:38:12 +0200

    adding of tags

--- src/lib/sidebar/Tags.svelte
+++ src/lib/sidebar/Tags.svelte
@@ -1,18 +1,45 @@
 <script>
+	import { supabase, user } from '$lib/supabase';
 	import Tag from './Tag.svelte';
 	import TagFolder from './TagFolder.svelte';
 
 	/** @type {import('./types').Tag[]} */
 	export let tags;
+
+	let addingTag = false;
+
+	let tag = '';
+	async function addTag() {
+		const { data, error } = await supabase.from('tags').insert({
+			user_id: $user?.id,
+			tag: tag
+		});
+
+		console.log(data, error);
+
+		addingTag = false;
+		tag = '';
+	}
 </script>
 
 <div class="w-64">
 	<div class="flex flex-row justify-between">
 		<h3>Tags folder</h3>
-		<button>+</button>
+
+		<button
+			on:click={() => {
+				addingTag = true;
+			}}>add +</button
+		>
 	</div>
 
 	<ul>
+		{#if addingTag}
+			<label for="tag">tag</label>
+			<input id="tag" type="text" bind:value={tag} />
+			<button on:click={addTag}>add +</button>
+		{/if}
+
 		{#each tags as tag}
 			<li>
 				{#if tag.tags}
