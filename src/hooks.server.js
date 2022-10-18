199a1c05575e758d6152df5ed89861c0a9edf603
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-10-18 13:04:21 +0200

    setup api stuff

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,28 @@
-// import { parse } from 'cookie';
-// import { supabase } from '$lib/supabase';
+import { supabase } from '$lib/supabase';
 
-// import { handleAuth } from '@supabase/auth-helpers-sveltekit';
+// /** @type {import('@sveltejs/kit').Handle} */
+// export async function handle({ event, resolve }) {
+// 	const session = event.cookies.get('session');
+
+// 	console.log(session);
+
+// 	if (!session) {
+// 		return await resolve(event);
+// 	}
+
+// 	const user = {};
+// 	// const user = await // get user from supabase
+
+// 	if (user) {
+// 		event.locals.user = {
+// 			user: 'username',
+// 			role: 'role'
+// 		};
+// 	}
+
+// 	return await resolve(event);
+// }
+
 // import { sequence } from '@sveltejs/kit/hooks';
 
 // export const handle = sequence(...handleAuth());
@@ -13,37 +34,4 @@
 // 		accessToken,
 // 		error
 // 	};
-// };
-
-/*
- ** https://kit.svelte.dev/docs/hooks#handle
- */
-// export const handle = async ({ event, resolve }) => {
-// 	const cookies = parse(event.request.headers.cookie || '');
-
-// 	console.log(cookies);
-
-// 	/* if there are cookies named `session` and `user`, grab the info */
-// 	// const cookies = await parse(event.request.headers.get('cookie') || '');
-// 	// const session = cookies.session ? JSON.parse(cookies.session) : null;
-// 	// const user = cookies.user ? JSON.parse(cookies.user) : null;
-
-// 	// console.log(session, cookies, user, event.request.headers.get('cookie'));
-
-// 	event.locals.foo = 'bar';
-
-// 	// event.locals.user = await getUserInformation(event.request.headers.get('cookie'));
-
-// 	/* set server-side auth for supabase */
-// 	// if (session) supabase.auth.setAuth(session.access_token);
-
-// 	/*
-// 	 ** use event.locals to securely store session data, like access tokens, for use in js endpoints;
-// 	 ** or for user data on the server-side.
-// 	 ** `.user` here is arbitrary - use what you'd like.
-// 	 */
-// 	// event.locals.user = user;
-
-// 	const response = await resolve(event);
-// 	return response;
 // };
--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,6 +1,7 @@
-/** @type {import('./$types').PageLoad} */
-export function load(event) {
-	const { locals } = event;
-	// console.log(locals);
-	// return locals.user ? { user: locals.user } : { user: null };
-}
+// /** @type {import('./$types').LayoutServerLoad} */
+// export function load({ locals }) {
+// 	console.log(locals);
+// 	// return locals.user ? { user: locals.user } : { user: null };
+
+// 	return locals.user;
+// }
