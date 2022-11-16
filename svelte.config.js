bd3da9e2f600e00a8ae273eaa0802b0a7147e68b
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-11-16 22:37:52 +0100

    fix: vercel deploy issue?

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
-import vercelAdapter from '@sveltejs/adapter-vercel';
-import staticAdapter from '@sveltejs/adapter-static';
+// import vercelAdapter from '@sveltejs/adapter-vercel';
+// import staticAdapter from '@sveltejs/adapter-static';
+import autoAdapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log(process.env.IS_DESKTOP ? 'building static' : 'building Vercel');
+// console.log(process.env.IS_DESKTOP ? 'building static' : 'building Vercel');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,13 +14,14 @@ const config = {
 
 	kit: {
 		// Change adapter based on build enviroment
-		adapter: process.env.IS_DESKTOP
-			? staticAdapter({
-					pages: 'electron/dist',
-					assets: 'electron/dist',
-					fallback: 'index.html'
-			  })
-			: vercelAdapter()
+		// adapter: process.env.IS_DESKTOP
+		// 	? staticAdapter({
+		// 			pages: 'electron/dist',
+		// 			assets: 'electron/dist',
+		// 			fallback: 'index.html'
+		// 	  })
+		// 	: vercelAdapter()
+		adapter: autoAdapter()
 
 		// Only build the endpoints in web enviroment
 		// This seems removed for now?
