3445c1ab8ed0394efcc2593c0a333596b0681c94
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-11-16 23:09:41 +0100

    beepboop

--- src/routes/notes/+page.js
+++ src/routes/notes/+page.js
@@ -1,4 +1,4 @@
-import { redirect } from '@sveltejs/kit';
+// import { redirect } from '@sveltejs/kit';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 
 /** @type {import('./$types').PageLoad} */
