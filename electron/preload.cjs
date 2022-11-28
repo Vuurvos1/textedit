0cf5fd59f6ad6dca7961ecd3121f94fe49838f49
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-11-28 08:26:46 +0100

    chore: remove old electron stuff

--- electron/preload.cjs
+++
@@ -1,8 +0,0 @@
-const { ipcRenderer, contextBridge } = require('electron');
-
-const WINDOW_API = {
-	GetVersion: (args) => ipcRenderer.invoke('get/version', args)
-};
-
-// window.api
-contextBridge.exposeInMainWorld('api', WINDOW_API);
