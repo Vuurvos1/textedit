ea2b3b4bf3cad0237de3c33ce170197b0e2bcebe
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-01-22 19:25:30 +0100

    wip merging tags into notes

--- src/routes/notes/+page.ts
+++ src/routes/notes/+page.ts
@@ -13,6 +13,23 @@ export const load: PageLoad = async (event) => {
 		};
 	}
 
+	// query notes and merge tags into notes using note_tags
+	// const newNotes = await supabaseClient.from('notes').select('*, tags!inner(*)'); // merge note tags into notes here
+	// console.log(newNotes.data);
+	// console.log(newNotes.data.map((n) => [n.title, n.tags]));
+
+	const newNoteTags = await supabaseClient
+		.from('note_tags')
+		.select('note_id!inner(id), id, tag_id (tag, id)');
+	// .eq('note_id.id', note.id);
+
+	console.log(newNoteTags.data);
+
+	// const { data, error } = await supabaseClient
+	// .from('note_tags')
+	// .select('note_id!inner(id), id, tag_id (tag, id)')
+	// .eq('note_id.id', note.id);
+
 	const tagsRes = await supabaseClient.from('tags').select('tag, id');
 
 	if (tagsRes.error) {
@@ -25,6 +42,14 @@ export const load: PageLoad = async (event) => {
 	if (notesRes.error) {
 		console.error(notesRes.error);
 	}
+
+	const mergedNotes = notesRes.data.map((n) => {
+		const noteTags = newNoteTags.data.filter((nt) => nt.note_id.id === n.id);
+		n.tags = noteTags.map((nt) => nt.tag_id.tag);
+		return n;
+	});
+
+	console.log(mergedNotes.map((n) => [n.title, n.tags]));
 
 	return {
 		notes: notesRes.data ? notesRes.data : [],
