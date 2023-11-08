01d5f833c86f8f2dfbc5cb3f0bfb04a58467f550
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-11-08 23:31:42 +0100

    blazingly fast reading of markdown files

---
+++ apps/desktop/src-tauri/src/markdown_file_list.rs
@@ -0,0 +1,29 @@
+extern crate walkdir;
+
+use walkdir::WalkDir;
+use std::path::PathBuf;
+
+pub fn list_markdown_files_in_folder(folder_path: &PathBuf) -> Vec<String> {
+    let markdown_files = WalkDir::new(&folder_path)
+        .into_iter()
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            if entry.file_type().is_file() {
+                if let Some(file_name) = entry.file_name().to_str() {
+                    if file_name.ends_with(".md") {
+                        Some(file_name.to_string())
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .map(|file| file)
+        .collect();
+
+    markdown_files
+}
