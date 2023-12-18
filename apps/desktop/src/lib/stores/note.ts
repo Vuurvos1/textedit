d58ba682b3f0b48a4379569c461386627e952546
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-12-18 20:10:52 +0100

    file saving

--- apps/desktop/src/lib/stores/note.ts
+++ apps/desktop/src/lib/stores/note.ts
@@ -1,14 +1,25 @@
-import { writable } from 'svelte/store';
-import { readTextFile, type FileEntry } from '@tauri-apps/api/fs';
+import { get, writable } from 'svelte/store';
+import { readTextFile, writeTextFile, type FileEntry } from '@tauri-apps/api/fs';
 
 function createNote() {
-	const { subscribe, update, set } = writable({
+	const store = writable({
 		path: '',
 		name: '',
 		content: ''
 	});
+	const { subscribe, update, set } = store;
+
+	async function save() {
+		const note = get(store);
+
+		if (!note.path || !note.content) return;
+
+		writeTextFile(note.path, note.content);
+	}
 
 	async function load(file: FileEntry) {
+		await save();
+
 		const fileContents = await readTextFile(file.path);
 
 		set({
@@ -23,6 +34,7 @@ function createNote() {
 		update,
 		set,
 		load,
+		save,
 		reset: () =>
 			set({
 				path: '',
--- apps/desktop/src/routes/+page.svelte
+++ apps/desktop/src/routes/+page.svelte
@@ -3,6 +3,11 @@
 	import Sidebar from '$lib/Sidebar.svelte';
 	import WordCounter from '$lib/WordCounter.svelte';
 	import { note } from '$lib/stores';
+	import { debounce } from '$lib/utils';
+
+	function saveNote() {
+		note.save();
+	}
 </script>
 
 <div class="flex h-full flex-row">
@@ -11,6 +16,7 @@
 	<main class="flex h-full w-full flex-col">
 		<div class="relative h-full overflow-hidden">
 			<textarea
+				on:keydown={debounce(saveNote, 2500)}
 				bind:value={$note.content}
 				class="h-full w-full resize-none border-none p-3 focus:outline-none"
 			></textarea>
