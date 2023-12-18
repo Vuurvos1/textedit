6a93b12e5c7342a6cff674dd78f2ccab6c0e3215
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-12-18 20:11:36 +0100

    highlight selected note

--- apps/desktop/src/lib/File.svelte
+++ apps/desktop/src/lib/File.svelte
@@ -13,6 +13,7 @@
 
 <!-- TODO add file icons / tags -->
 <button
+	class:bg-gray-200={$note.path === path}
 	class="mb-1 w-full truncate rounded px-4 py-1 pl-4 text-left text-sm transition duration-100 hover:bg-gray-300/50"
 	use:draggable={JSON.stringify({ name, path })}
 	on:click={() => loadFile()}
