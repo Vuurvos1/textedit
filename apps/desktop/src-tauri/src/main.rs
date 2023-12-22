8c06129c12660fa16a0ebc3e0db4320e0c35965f
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2023-12-22 22:05:38 +0100

    cleanup rust

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,27 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod markdown_file_list;
-
-use std::path::PathBuf;
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[tauri::command]
-fn search_markdown_files(selected_folder: String) -> Vec<String> {
-    let selected_folder_path = PathBuf::from(selected_folder);
-    let markdown_files = markdown_file_list::list_markdown_files_in_folder(&selected_folder_path);
-    markdown_files
-}
-
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .invoke_handler(tauri::generate_handler![search_markdown_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
