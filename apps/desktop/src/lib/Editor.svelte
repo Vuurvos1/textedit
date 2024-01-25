ca90cabaae9b9332c184a3ed2cbf61812913733b
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-01-25 22:52:37 +0100

    code mirror hypermd

--- apps/desktop/src/lib/Editor.svelte
+++ apps/desktop/src/lib/Editor.svelte
@@ -1,26 +1,32 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
-	import { SvelteNodeViewRenderer, createEditor, Editor, EditorContent } from 'svelte-tiptap';
-	import StarterKit from '@tiptap/starter-kit';
-	import TaskItem from '@tiptap/extension-task-item';
-	import TaskList from '@tiptap/extension-task-list';
-	import Image from '@tiptap/extension-image';
-	import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
-	import CodeBlockComponent from '$lib/editor/Codeblock.svelte';
-	import { lowlight } from 'lowlight';
-	import { Markdown } from 'tiptap-markdown';
-	import CharacterCount from '@tiptap/extension-character-count';
+	// import { SvelteNodeViewRenderer, createEditor, Editor, EditorContent } from 'svelte-tiptap';
+	// import StarterKit from '@tiptap/starter-kit';
+	// import TaskItem from '@tiptap/extension-task-item';
+	// import TaskList from '@tiptap/extension-task-list';
+	// import Image from '@tiptap/extension-image';
+	// import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
+	// import CodeBlockComponent from '$lib/editor/Codeblock.svelte';
+	// import { lowlight } from 'lowlight';
+	// import { Markdown } from 'tiptap-markdown';
+	// import CharacterCount from '@tiptap/extension-character-count';
 	// import { note, noteDirty, notes, updateNote } from '$lib/stores';
-	import type { Readable } from 'svelte/store';
+	// import type { Readable } from 'svelte/store';
+
+	import * as HyperMD from 'hypermd';
+
+	// import CodeMirror from 'codemirror';
+	// codemirror.defaults
 
 	// import { saveNote } from '$lib/utils';
 
-	import 'highlight.js/styles/github-dark.css';
+	// import 'highlight.js/styles/github-dark.css';
+	// import Codeblock from '$lib/editor/Codeblock.svelte';
 
-	let editor: Readable<Editor>;
+	// let editor: Readable<Editor>;
 
 	// TODO: there might be a better way to do this
-	let noteChanged = true;
+	// let noteChanged = true;
 
 	// const unsubscribe = updateNote.subscribe(() => {
 	// 	if (editor && $note !== undefined) {
@@ -30,92 +36,112 @@
 	// 	}
 	// });
 
-	function debounce(func: Function, delay = 250) {
-		// let timeout: NodeJS.Timeout;
+	// function debounce(func: Function, delay = 250) {
+	// 	// let timeout: NodeJS.Timeout;
 
-		return (...args: any) => {
-			// $note.content = $editor.storage.markdown.getMarkdown(); // always update store
+	// 	return (...args: any) => {
+	// 		// $note.content = $editor.storage.markdown.getMarkdown(); // always update store
 
-			if (!noteChanged) {
-				// $noteDirty = true;
-				// $note.updated_at = new Date().toISOString();
-				// $notes = $notes;
-			}
+	// 		if (!noteChanged) {
+	// 			// $noteDirty = true;
+	// 			// $note.updated_at = new Date().toISOString();
+	// 			// $notes = $notes;
+	// 		}
 
-			if (noteChanged) {
-				noteChanged = false;
-			}
+	// 		if (noteChanged) {
+	// 			noteChanged = false;
+	// 		}
 
-			// clearTimeout(timeout);
-			// timeout = setTimeout(() => {
-			// 	func(...args);
-			// }, delay);
-		};
-	}
+	// 		// clearTimeout(timeout);
+	// 		// timeout = setTimeout(() => {
+	// 		// 	func(...args);
+	// 		// }, delay);
+	// 	};
+	// }
 
-	function save() {
-		console.info('Saving...');
-		// saveNote();
-		// localStorage.setItem('note-data', JSON.stringify($notes));
-	}
+	// function save() {
+	// 	console.info('Saving...');
+	// 	// saveNote();
+	// 	// localStorage.setItem('note-data', JSON.stringify($notes));
+	// }
 
-	onMount(async () => {
-		editor = createEditor({
-			extensions: [
-				StarterKit.configure({
-					codeBlock: false
-				}),
-				TaskList,
-				TaskItem.configure({
-					nested: true
-				}),
-				Image,
-				CodeBlockLowlight.extend({
-					addNodeView() {
-						return SvelteNodeViewRenderer(CodeBlockComponent);
-					}
-				}).configure({ lowlight }),
-				Markdown,
-				CharacterCount
-			],
-			editorProps: {
-				attributes: {
-					// prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5
-					class: 'prose lg:prose-lg focus:outline-none w-full max-w-[initial]'
-				}
-			},
-			onUpdate: debounce(save, 3500)
-			// content: $note.content || ''
+	// onMount(async () => {
+	// 	editor = createEditor({
+	// 		extensions: [
+	// 			StarterKit.configure({
+	// 				codeBlock: false
+	// 			}),
+	// 			TaskList,
+	// 			TaskItem.configure({
+	// 				nested: true
+	// 			}),
+	// 			Image,
+	// 			CodeBlockLowlight.extend({
+	// 				addNodeView() {
+	// 					return SvelteNodeViewRenderer(CodeBlockComponent);
+	// 				}
+	// 			}).configure({ lowlight }),
+	// 			Markdown,
+	// 			CharacterCount
+	// 		],
+	// 		editorProps: {
+	// 			attributes: {
+	// 				// prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5
+	// 				class: 'prose lg:prose-lg focus:outline-none w-full max-w-[initial]'
+	// 			}
+	// 		},
+	// 		onUpdate: debounce(save, 3500)
+	// 		// content: $note.content || ''
+	// 	});
+	// });
+
+	// onDestroy(() => {
+	// 	if ($editor) {
+	// 		$editor.destroy();
+	// 	}
+	// 	// unsubscribe();
+	// });
+
+	let myTextarea: HTMLTextAreaElement;
+
+	onMount(() => {
+		// var myTextarea = document.getElementById('input-area');
+
+		console.log(myTextarea, HyperMD);
+		const editor = HyperMD.fromTextArea(myTextarea, {
+			lineNumbers: false,
+			gutters: ['CodeMirror-foldgutter', 'HyperMD-goback']
 		});
-	});
 
-	onDestroy(() => {
-		if ($editor) {
-			$editor.destroy();
-		}
-		// unsubscribe();
+		console.log(editor);
 	});
 </script>
 
-<div class="editor h-full overflow-y-auto bg-[var(--background-primary)] p-4">
-	<EditorContent editor={$editor} />
-</div>
+<!-- <div class="editor h-full overflow-y-auto bg-[var(--background-primary)] p-4">
+</div> -->
+<!-- <EditorContent editor={$editor} /> -->
 
-<div class="flex justify-end gap-4 border-t px-4 py-1 text-gray-500">
-	<!-- <span>
+<!-- <div class="flex justify-end gap-4 border-t px-4 py-1 text-gray-500"> -->
+<textarea class="h-full" bind:this={myTextarea}></textarea>
+
+<!-- <span>
 			 lines: {$editor?.storage?.characterCount?.lines() || 0} 
 		</span> -->
 
-	<!-- <span>
+<!-- <span>
 		characters: {$editor?.storage?.characterCount?.characters() || 0}
 	</span>
 
 	<span>
 		words: {$editor?.storage?.characterCount?.words() || 0}
 	</span> -->
-</div>
+<!-- </div> -->
 
 <style lang="postcss">
+	:global(.CodeMirror) {
+		@apply h-full;
+	}
+
 	/* Basic editor styles */
 	.editor :global(.ProseMirror > * + *) {
 		@apply mt-3;
